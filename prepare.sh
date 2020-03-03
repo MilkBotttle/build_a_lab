42efffcb4dc6aa15016728324fcf957cf6f5d4b2
milkbotttle <milkbottles1990812@gmail.com>
2020-03-03 11:32:34 +0800

    vir_net add autostart, fix prepare.sh error

--- prepare.sh
+++ prepare.sh
@@ -7,6 +7,9 @@ VAGRANT_RPM=/tmp/vagrant.rpm
 
 yum install qemu libvirt libvirt-devel ruby-devel gcc qemu-kvm make automake gcc gcc-c++ kernel-devel python-devel libvirt-devel -y
 
+systemctl start libvirtd
+systemctl enable libvirtd
+
 # Install pip
 curl https://bootstrap.pypa.io/get-pip.py | python
 
@@ -17,7 +20,7 @@ source $VENV/bin/activate
 pip install ansible lxml selinux libvirt-python==5.0.0 virtualbmc==1.4.0
 
 # Install vagrant
-curl -o https://releases.hashicorp.com/vagrant/2.2.7/vagrant_2.2.7_x86_64.rpm $VAGRANT_RPM
+curl https://releases.hashicorp.com/vagrant/2.2.7/vagrant_2.2.7_x86_64.rpm -o $VAGRANT_RPM
 yum install -y $VAGRANT_RPM
 
 vagrant plugin install vagrant-libvirt
